9f4e23ca5300fa8d18d9a7df8b29e62e108fbc57
Kim JH Desktop <kim_jh6859@naver.com>
2023-10-04 02:02:07 +0900

    디자인 수정

--- src/Banner.js
+++ src/Banner.js
@@ -6,8 +6,36 @@ import { Link, useNavigate, useLocation } from 'react-router-dom';
 function Banner() {
     const navigate = useNavigate();
 
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        // 컴포넌트 언마운트 시 이벤트 리스너 정리
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <div className={styles.container}>
+        <div className={`${styles.container} ${scrolled ? styles.scrolled : ''}`} >
+            <div className={`${styles.goToTop} ${scrolled ? styles.scrolled : ''}`}
+                onClick={() => {
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth',
+                    });
+                }}>
+                ^
+            </div>
             <div className={styles.title} onClick={() => navigate('/')}>
                 Portfolio
             </div>
@@ -26,6 +54,7 @@ function Banner() {
                 </div>
             </div>
         </div>
+
     );
 }
 
